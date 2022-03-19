07b63477915ddcac3fdbb8c54c94d5947ce4078e
DenisVorop <saskeazaske@icloud.com>
2022-03-19 23:13:07 +0300

    add printFile

--- src/Components/Page/Plan/Plan.jsx
+++ src/Components/Page/Plan/Plan.jsx
@@ -1,34 +1,41 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import ReactToPrint from "react-to-print";
 
 import './plan.scss';
 
 import print from '../../../assets/img/print.svg';
+import PrintFile from '../../PrintFile/PrintFile';
+import Records from '../Records/Records';
 
-class Plan extends Component {
-    render() {
-        return (
-            <div className="plan" ref={el => (this.componentRef = el)}>
-                <div className="plan__container">
-                    <div className="plan__header">
-                        <ReactToPrint
-                            trigger={() => {
-                                return (
-                                    <div className="plan__print">
-                                        <img src={print} alt="print" />
-                                    </div>
-                                )
-                            }}
-                            content={() => this.componentRef}
-                            documentTitle='new document'
-                            pageStyle='print'
-                        />
-                        <div className="plan__label">План амбулаторного приема</div>
-                    </div>
+const Plan = () => {
+
+    const componentRef = useRef();
+
+    return (
+        <div className="plan">
+            <div className="plan__container">
+                <div className="plan__header">
+                    <ReactToPrint
+                        trigger={() => {
+                            return (
+                                <div className="plan__print">
+                                    <img src={print} alt="print" />
+                                </div>
+                            )
+                        }}
+                        content={() => componentRef.current}
+                        documentTitle='new document'
+                        pageStyle='print'
+                    />
+                    <div className="plan__label">План амбулаторного приема</div>
                 </div>
             </div>
-        )
-    }
+            <div style={{ display: "none" }}>
+                <Records ref={componentRef} />
+                <PrintFile ref={componentRef} />
+            </div>
+        </div>
+    )
 }
 
 export default Plan;
---
+++ src/Components/PrintFile/PrintFile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PrintFile extends Component {
+    render() {
+        return (
+            <div>hey</div>
+        )
+    }
+}
+
+export default PrintFile;
