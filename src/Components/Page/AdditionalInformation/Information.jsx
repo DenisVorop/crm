08964e0bb8b12edee4c335091807db5fe3d54a5c
DenisVorop <saskeazaske@icloud.com>
2022-03-24 01:26:27 +0300

    change popup for last record (before was for info) && add info layout for record

--- src/Components/Page/AdditionalInformation/Information.jsx
+++ src/Components/Page/AdditionalInformation/Information.jsx
@@ -1,67 +1,81 @@
+import { useState } from 'react';
+
+import Popup from '../Popup/Popup';
+import Last from '../LastsRecords/Last';
+
 import eye from '../../../assets/img/eye.svg';
-import copy from '../../../assets/img/copy.svg';
+// import copy from '../../../assets/img/copy.svg';
 
 import './information.scss';
 
-const Information = () => {
+const Information = ({ tableRef }) => {
+
+    const [popupActive, setPopupActive] = useState(false);
+
     return (
         <div className="information">
             <div className="information__container">
-                <div className='information__body'>
-                    <div className="information__title">Дополнительная информация</div>
+                <div className='information__body' ref={tableRef}>
+                    {/* <div className="information__title">Дополнительная информация</div>
                     <div className="information__card card-information">
                         <div className="information__label">Номер карты пациента</div>
                         <div className="card-information__number">
                             <div className="card-information__num">236713</div>
                             <div className="card-information__copy">
-                                <a href='#' ><img src={copy} alt="copy" /></a>
+                                <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)}><img src={copy} alt="copy" /></span>
                             </div>
                         </div>
-                    </div>
-                    <div className="information__lasts">
+                    </div> */}
+                    <div className="information__lasts lasts-information">
                         <div className="information__label bold">Последние приемы</div>
-                        <div className="information__last last-information">
-                            <div className="last-information__doctor">
-                                <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
-                                <div className="last-information__name">Иванова Г.Ю.</div>
-                            </div>
-                            <div className="last-information__item">
-                                <div className="last-information__date">14.02.2021</div>
-                                <div className="last-information__building">Название филиала №2</div>
-                                <div className="last-information__see">
-                                    <a href='#' ><img src={eye} alt="eye" /></a>
+                        <div className="lasts-information__records">
+                            <div className="information__last last-information">
+                                <div className="last-information__doctor">
+                                    <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
+                                    <div className="last-information__name">Иванова Г.Ю.</div>
+                                </div>
+                                <div className="last-information__item">
+                                    <div className="last-information__date">14.02.2021</div>
+                                    <div className="last-information__building">Название филиала №2</div>
+                                    <div className="last-information__see">
+                                        <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)}><img src={eye} alt="eye" /></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="information__last last-information chetnoe">
-                            <div className="last-information__doctor">
-                                <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
-                                <div className="last-information__name">Иванова Г.Ю.</div>
-                            </div>
-                            <div className="last-information__item">
-                                <div className="last-information__date">14.02.2021</div>
-                                <div className="last-information__building">Название филиала №2</div>
-                                <div className="last-information__see">
-                                    <a href='#' ><img src={eye} alt="eye" /></a>
+                            {/* <div className="information__last last-information chetnoe"> */}
+                            <div className="information__last last-information">
+                                <div className="last-information__doctor">
+                                    <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
+                                    <div className="last-information__name">Иванова Г.Ю.</div>
+                                </div>
+                                <div className="last-information__item">
+                                    <div className="last-information__date">14.02.2021</div>
+                                    <div className="last-information__building">Название филиала №2</div>
+                                    <div className="last-information__see">
+                                        <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)}><img src={eye} alt="eye" /></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="information__last last-information">
-                            <div className="last-information__doctor">
-                                <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
-                                <div className="last-information__name">Иванова Г.Ю.</div>
-                            </div>
-                            <div className="last-information__item">
-                                <div className="last-information__date">14.02.2021</div>
-                                <div className="last-information__building">Название филиала №2</div>
-                                <div className="last-information__see">
-                                    <a href='#' ><img src={eye} alt="eye" /></a>
+                            <div className="information__last last-information">
+                                <div className="last-information__doctor">
+                                    <div className="last-information__label">Медицинская сестра (медбрат) участковая</div>
+                                    <div className="last-information__name">Иванова Г.Ю.</div>
+                                </div>
+                                <div className="last-information__item">
+                                    <div className="last-information__date">14.02.2021</div>
+                                    <div className="last-information__building">Название филиала №2</div>
+                                    <div className="last-information__see">
+                                        <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)}><img src={eye} alt="eye" /></span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Popup popupActive={popupActive} setPopupActive={setPopupActive} >
+                <Last />
+            </Popup>
         </div>
     )
 }
--- src/Components/Page/Records/Record.jsx
+++ src/Components/Page/Records/Record.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import info from '../../../assets/img/Info.svg';
 import Information from '../AdditionalInformation/Information';
-import Popup from '../Popup/Popup';
+// import Popup from '../Popup/Popup';
 
 const Record = ({ name, age, sex, status }) => {
-    const [popupActive, setPopupActive] = useState(false);
-
     let statusStyle = {};
 
     switch (status) {
@@ -16,22 +14,48 @@ const Record = ({ name, age, sex, status }) => {
         default: { statusStyle = { background: 'yellow', color: 'black' }; break; }
     }
 
+    const [infoVisible, setInfoVisible] = useState(false);
+    const infoRef = useRef();
+    const tableRef = useRef();
+
+    const toggleInfoVisible = () => {
+        setInfoVisible(!infoVisible);
+    };
+
+    const handleOutsideClick = (event) => {
+        const path = event.path || (event.composedPath && event.composedPath());
+        if (!path.includes(infoRef.current) && !path.includes(tableRef.current)) {
+            setInfoVisible(false);
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick);
+    }, []);
+
     return (
         <>
             <div className="records__row">
-                <div className="records__column time-column">10:30</div>
-                <div className="records__column name-column">{name}</div>
-                <div className="records__column sex-column">{sex}, {age}</div>
-                <div className="records__column-item">
-                    <div className="records__column status-column" style={statusStyle}>{status}</div>
-                    <div className="records__column info-column">
-                        <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)} ><img src={info} alt="info" /></span>
+                <div className="records__person" ref={infoRef}>
+                    <div className="records__column time-column">10:30</div>
+                    <div className="records__column name-column">{name}</div>
+                    <div className="records__column sex-column">{sex}, {age}</div>
+                    <div className="records__column-item">
+                        <div className="records__column status-column" style={statusStyle}>{status}</div>
+                        <div className="records__column info-column">
+                            {/* <span style={{ cursor: 'pointer' }} onClick={() => setPopupActive(true)} ><img src={info} alt="info" /></span> */}
+                            <span onClick={toggleInfoVisible} style={{ cursor: 'pointer' }}><img src={info} alt="info" /></span>
+                        </div>
                     </div>
                 </div>
+                <div className={!infoVisible ? 'info-visible' : 'info-visible active'}>
+                    {infoVisible && <Information tableRef={tableRef} />}
+                </div>
             </div>
-            <Popup popupActive={popupActive} setPopupActive={setPopupActive}>
-                <Information />
-            </Popup>
+            {/* <Popup popupActive={popupActive} setPopupActive={setPopupActive}>
+            <Information />
+            </Popup> */}
+            {/* onClick={e => e.stopPropagation() */}
         </>
     )
 }
