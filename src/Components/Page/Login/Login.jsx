5a228eeea63dc9b08254a98f0c93a5f46d66d33f
DenisVorop <saskeazaske@icloud.com>
2022-03-24 01:49:02 +0300

    add formik for login

--- src/Components/Page/Login/Login.jsx
+++ src/Components/Page/Login/Login.jsx
@@ -1,23 +1,61 @@
+import { Form, Formik } from 'formik';
 import './login.scss'
+
 
 const Login = () => {
     return (
-        <div className="login">
-            <div className="login__container">
-                <div className="login__body">
-                    <div className="login__title">Введите данные для входа</div>
-                    <div className="login__login login-login">
-                        <div className="login-login__label login-label">Имя пользователя</div>
-                        <input type="text" className="login-login__input login-input" placeholder='Введите имя пользователя' />
+        <Formik
+            initialValues={{
+                login: '',
+                password: '',
+            }}
+            validateOnBlur={false}
+            validateOnChange={false}
+            onSubmit={(values) => {
+                console.log(values)
+            }}
+        // validationSchema={validationLogin}
+        >
+            {({ values, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+                <Form>
+                    <div className="login">
+                        <div className="login__container">
+                            <div className="login__body">
+                                <div className="login__title">Введите данные для входа</div>
+                                <div className="login__login login-login">
+                                    <div className="login-login__label login-label">Имя пользователя</div>
+                                    <input
+                                        className="login-login__input login-input"
+                                        placeholder='Введите имя пользователя'
+                                        type="login"
+                                        name='login'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </div>
+                                <div className="login__password login-password">
+                                    <div className="login-password__label login-label">Пароль</div>
+                                    <input
+                                        className="login-password__input login-input"
+                                        placeholder='Введите пароль'
+                                        type="password"
+                                        name='password'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </div>
+                                <button
+                                    className="login__button button"
+                                    disabled={!isValid && !dirty}
+                                    onClick={handleSubmit}
+                                    type='submit'
+                                >Войти в систему</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="login__password login-password">
-                        <div className="login-password__label login-label">Пароль</div>
-                        <input type="text" className="login-password__input login-input" placeholder='Введите пароль' />
-                    </div>
-                    <a href="#" className="login__button button">Войти в систему</a>
-                </div>
-            </div>
-        </div>
+                </Form>
+            )}
+        </Formik>
     )
 }
 
