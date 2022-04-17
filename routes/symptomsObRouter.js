8b2adb724a2ca01e6fdd54d3302f7e369e2011da
Denis Voropaev <95316053+DenisVorop@users.noreply.github.com>
2022-04-17 15:54:34 +0300

    Delete routes directory

--- routes/symptomsObRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const symptomsObController = require('../controllers/symptomsObController')
-
-const router = new Router()
-
-router.post('/', symptomsObController.create)
-router.get('/', symptomsObController.getAll)
-
-module.exports = router
--- routes/symptomsSubRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const symptomSubController = require('../controllers/symptomSubController')
-
-const router = new Router()
-
-router.post('/', symptomSubController.create)
-router.get('/', symptomSubController.getAll)
-
-module.exports = router
